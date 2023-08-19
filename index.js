10e8675c37722047f21c958c5d23b952c3c74e05
PeterJanasz <petertjanasz@gmail.com>
2023-08-19 13:06:27 -0700

    created init function and write me file. Added circle example in example folder.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const {Circle, Square, Triangle} = require('./lib/shapes');
+//const {Circle, Square, Triangle} = require('./lib/shapes');
+const generateLogo = require('./lib/shapes');
+
+const colorKeywords = []
 
 const questions = [
 {
@@ -14,9 +17,9 @@ const questions = [
     name: 'shapeColor',
     message: 'What color would you like your shape to be?',
     type: 'input',
-    validate: (answer) => {
+    // validate: (answer) => {
 
-    }
+    // }
 
 },
 {
@@ -34,9 +37,34 @@ const questions = [
     name: 'textColor',
     message: 'What color would you like your text to be?',
     type: 'input'
-    validate: (answer) => {
-        
-    }
+    // validate: (answer) => {
+
+    // }
 }
 
-]
+];
+
+// function createLogo(response) {
+//     const svg = generateLogo(response);
+//     fs.writeFile(fileName, svg, ()=> console.log('Generated logo.svg'));
+// }
+
+function writeToFile(fileName, data) {
+    fs.writeFileSync(fileName, data, 'utf-8');
+    console.log(`Logo saved to ${fileName}`);
+}
+
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((response) => {
+            const svgData = generateLogo(response);
+            writeToFile('logo.svg', svgData);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+}
+
+init();
+module.exports = questions;
