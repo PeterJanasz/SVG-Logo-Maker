68b359144cc9a35eccbe1ac3c8ef202abe79fe77
PeterJanasz <petertjanasz@gmail.com>
2023-08-20 12:16:27 -0700

    moved generateLogo to new file to work properly and completed test sucessfuly.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const colorKeywords = require('./lib/colorKeywords');
-const generateLogo = require('./lib/shapes');
+const generateLogo = require('./lib/generateLogo');
 
 const questions = [
     {
