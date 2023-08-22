4bd053e4c4d37739fe86b6e18972544d6836acab
PeterJanasz <petertjanasz@gmail.com>
2023-08-21 18:24:06 -0700

    moved questions to its own file.

--- index.js
+++ index.js
@@ -1,61 +1,63 @@
 const inquirer = require('inquirer'); // For user input prompts
 const fs = require('fs'); // For file system operations
-const colorKeywords = require('./lib/colorKeywords'); // List of color keywords
+// const colorKeywords = require('./lib/colorKeywords'); // List of color keywords
 const generateLogo = require('./lib/generateLogo'); // Function to generate SVG logo
+const questions = require('./lib');
 
-console.log(`
-Welcome to Logo Generator!
-Follow the steps to create your logo.
--------------------------------
-`);
-// Array of questions for user 
-const questions = [
-    {
-        name: 'shape',
-        message: 'What shape would you like your logo to be?',
-        type: 'list',
-        choices: ['Circle', 'Square', 'Triangle'],
-    },
-    {
-        name: 'shapeColor',
-        message: 'What color would you like your shape to be?',
-        type: 'input',
-         validate: (answer) => {
-            // Convert the color input to capitalized form to match colorKeywords
-            function capitalizeFirstLetter(answerStr) {
-                return answerStr.charAt(0).toUpperCase() + answerStr.slice(1);
-            }
-            const capitalizedColor = capitalizeFirstLetter(answer);
-            const validateColor = colorKeywords.includes(capitalizedColor);
-            return validateColor ? true : "Please enter a valid color."
-        }
-    },
-    {
-        name: 'text',
-        message: 'What text would you like in your shape? (No more than 3 character)',
-        type: 'input',
-        validate: (answer) => {
-            if (answer.length > 3) {
-                return "Text must be three characters or less! Please try again.";
-            }
-            return true;
-        }
-    },
-    {
-        name: 'textColor',
-        message: 'What color would you like your text to be?',
-        type: 'input',
-        validate: (answer) => {
-            function capitalizeFirstLetter(answerStr) {
-                return answerStr.charAt(0).toUpperCase() + answerStr.slice(1);
-            }
-            const capitalizedColor = capitalizeFirstLetter(answer);
-            const validateColor = colorKeywords.includes(capitalizedColor);
-            return validateColor ? true : "Please enter a valid color."
-        }
-    }
+// console.log(`
+// Welcome to Logo Generator!
+// Follow the steps to create your logo.
+// -------------------------------
+// `);
+// // Array of questions for user 
+// const questions = [
+//     {
+//         name: 'shape',
+//         message: 'What shape would you like your logo to be?',
+//         type: 'list',
+//         choices: ['Circle', 'Square', 'Triangle'],
+//     },
+//     {
+//         name: 'shapeColor',
+//         message: 'What color would you like your shape to be?',
+//         type: 'input',
+//          validate: (answer) => {
+//             // Convert the color input to capitalized form to match colorKeywords
+//             function capitalizeFirstLetter(answerStr) {
+//                 return answerStr.charAt(0).toUpperCase() + answerStr.slice(1);
+//             }
+//             const capitalizedColor = capitalizeFirstLetter(answer);
+//             const validateColor = colorKeywords.includes(capitalizedColor);
+//             return validateColor ? true : "Please enter a valid color."
+//         }
+//     },
+//     {
+//         name: 'text',
+//         message: 'What text would you like in your shape? (No more than 3 character)',
+//         type: 'input',
+//         validate: (answer) => {
+//             if (answer.length > 3) {
+//                 return "Text must be three characters or less! Please try again.";
+//             }
+//             return true;
+//         }
+//     },
+//     {
+//         name: 'textColor',
+//         message: 'What color would you like your text to be?',
+//         type: 'input',
+//         validate: (answer) => {
+//             function capitalizeFirstLetter(answerStr) {
+//                 return answerStr.charAt(0).toUpperCase() + answerStr.slice(1).toLowerCase();
+//             }
+//             const capitalizedColor = capitalizeFirstLetter(answer);
+//             const validateColor = colorKeywords.includes(capitalizedColor);
+            
+//             return validateColor ? true : "Please enter a valid color."
+//         }
+//     }
 
-];
+// ];
 // Function to write data to a file
 function writeToFile(fileName, data) {
     fs.writeFileSync(fileName, data, 'utf-8');
